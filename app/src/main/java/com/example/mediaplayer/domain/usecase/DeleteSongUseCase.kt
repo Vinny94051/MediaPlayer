00569f402f4c6e9cd8c2b7cdf24e8c5000626fa4
Vladislav <37775244+Vinny94051@users.noreply.github.com>
2022-02-16 19:35:44 +0300

    Update DeleteSongUseCase.kt

--- app/src/main/java/com/example/mediaplayer/domain/usecase/DeleteSongUseCase.kt
+++ app/src/main/java/com/example/mediaplayer/domain/usecase/DeleteSongUseCase.kt
@@ -3,9 +3,11 @@ package com.example.mediaplayer.domain.usecase
 import com.example.mediaplayer.data.model.Song
 import com.example.mediaplayer.domain.repository.PlaylistRepository
 
-class DeleteSongUseCase(private val playlistRepository: PlaylistRepository) {
+interface DeleteSongUseCase {
+    fun deleteSong(song: Song)
+}
 
-
+class DeleteSongUseCaseImpl(private val playlistRepository: PlaylistRepository): DeleteSongUseCase {
     fun deleteSongItem(song: Song) {
         playlistRepository.delete(song)
     }
